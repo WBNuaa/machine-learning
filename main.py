b4c87f41ffe50afef3a7f410ac12c42753d69df4
bujidao1 <3218385100@qq.com>
2021-01-19 15:03:48 +0800

    master

--- main.py
+++ main.py
@@ -12,21 +12,31 @@ import translate as tr
 #中文 zh
 language = ['zh','en','fra','spa','jp','ru','kor','de']
 
-def transform(content,sourse,destination):#蜕变关系
+def sample():
+    #测试用例生成（语种的选择）
+    return 
+
+#蜕变关系
+def transform(content,sourse,destination,relationship):
     return
 
-def translate(content,sourse,destination):#翻译
-    
-    return
+def translate(content,sourse,destination,tool):#翻译
+    if tool == 'baidu':
+        result = tr.translate_baidu(content,sourse,destination)
+    elif tool == 'youdao':
+        result = tr.translate_youdao(content,sourse,destination)
+    else:
+        result = tr.translate_google(content,sourse,destination)
+    return result
 
 def analyse():#结果对比分析
     return 
 
 if __name__ =='__main__':
-    content = "我喜欢你"
-    sourse = 'zh'
-    destination = 'en'
-    tr.translate_baidu(content,sourse,destination)
-    tr.translate_youdao(content,sourse,destination)
-    tr.translate_google(content,sourse,destination)
+    content = "我喜欢你" #翻译内容
+    sourse = 'zh' #源语言
+    destination = 'en' #目标语言
+    target = 'baidu' #测试系统
+    transform_relationship = 'baidu' #蜕变关系
+    layer = 2 #蜕变层数
 
