02668196cb871ca9f81b2a932510ddefd533612b
bujidao1 <3218385100@qq.com>
2021-01-23 10:29:10 +0800

    master

--- main.py
+++ main.py
@@ -2,25 +2,94 @@ import similarity as si
 import translate as tr
 
 #语种
-#法语 fra
-#西班牙语 spa
-#日语 jp
-#俄语 ru
-#韩语 kor
-#德语 de
-#英语 en
-#中文 zh
-language = ['zh','en','fra','spa','jp','ru','kor','de']
 
-def sample():
+##俄语 ru
+#希腊语 el
+##英语 en
+##中文 zh
+##苗语 hmn
+##葡萄牙语 pt
+##泰语 th
+##德语 de
+
+language = ['zh','en','ru','el','hmn','pt','th','de']
+software = ['baidu','youdao','google']
+
+def sample(sourse,destination,layer):
     #测试用例生成（语种的选择）
-    return 
+    info = {}
+    list = []   #蜕变语种
+    #info['target'] = target
+    info['sourse'] = sourse
+    info['destination'] = destination
+    #info['transform_relationship'] = transform_relationship
+    info['layer'] = int(layer)
+    #print(info)
+    if info['layer'] == 1:
+      # for i in range (info[layer]):
+          for j in range(len(language)):
+              
+              if language[j] != info['sourse'] and language[j] != info['destination']:
+                  temp1_1 = language[j]
+                  list.append({temp1_1})
+
+    elif info['layer'] == 2:
+        # for i in range(info[layer]):
+            for j in range(len(language)):
+
+                if language[j] != info['destination'] and language[j] != info['sourse']:
+                    temp2_1 = language[j]
+                    for k in range(len(language)):
+                        if language[k] != info['destination'] and language[k] != info['sourse'] and language[k] != temp2_1:
+                            temp2_2 = language[k]
+                            list.append({temp2_1,temp2_2})
+
+    elif info['layer'] == 3:
+        # for i in range(info[layer]):
+            for j in range(len(language)):
+                if language[j] != info['destination'] and language[j] != info['sourse']:
+                    temp3_1 = language[j]
+                    for k in range(len(language)):
+                        if language[k] != info['destination'] and language[k] != info['sourse'] and language[k] != temp3_1:
+                            temp3_2 = language[k]
+                            for l in range(len(language)):
+                                if language[l] != info['destination'] and language[l] != info['sourse'] and language[l] != temp3_1 and language[l] != temp3_2:
+                                    temp3_3 = language[l]
+                                    list.append({temp3_1,temp3_2,temp3_3})
+    
+
+    else:
+        print("蜕变层数错误请重新检查!")
+
+
+    #print(list)
+    return list
 
 #蜕变关系
-def transform(content,sourse,destination,relationship):
-    return
+def transform(content,target,sourse,destination,transform_relationship,layer):
+    translateresult = []#存放翻译的结果
+    result = translate(content,sourse,destination,target)
+    translateresult.append(result)
 
-def translate(content,sourse,destination,tool):#翻译
+    result = sample(sourse,destination,layer)#获取蜕变测试用例
+    #翻译
+    for tmp in result:
+        #将字典转化为列表
+        tmp = list(tmp)
+        print(tmp[0])
+        content_m = translate(content,sourse,tmp[0],transform_relationship)
+        print(content_m)
+        i = 0
+        for i in range(1,len(tmp)):
+            content_m = translate(content_m,tmp[i-1],tmp[i],transform_relationship)
+        content_m = translate(content_m,tmp[i],destination,transform_relationship)
+        translateresult.append(content_m)
+
+    print(translateresult)
+    return translateresult
+
+#翻译
+def translate(content,sourse,destination,tool):
     if tool == 'baidu':
         result = tr.translate_baidu(content,sourse,destination)
     elif tool == 'youdao':
@@ -36,7 +105,8 @@ if __name__ =='__main__':
     content = "我喜欢你" #翻译内容
     sourse = 'zh' #源语言
     destination = 'en' #目标语言
-    target = 'baidu' #测试系统
-    transform_relationship = 'baidu' #蜕变关系
+    target = 'google' #测试系统
+    transform_relationship = 'google' #蜕变关系
     layer = 2 #蜕变层数
+    transform(content,target,sourse,destination,transform_relationship,layer)
 
--- translate.py
+++ translate.py
@@ -2,9 +2,11 @@ import urllib.request
 import urllib.parse
 import json
 import hashlib
+import random
+import time
 from google_trans_new import google_translator
 
-# 可更改的有翻译的内容、语种
+# 百度翻译
 def translate_baidu(content,sourse,destination):
     URL='http://api.fanyi.baidu.com/api/trans/vip/translate'
     From_Data={}  #创建From_Data字典，存储向服务器发送的data
@@ -12,28 +14,30 @@ def translate_baidu(content,sourse,destination):
     From_Data['to']=destination #翻译的语种
     From_Data['q']=content     #要翻译的数据
     From_Data['appid']='20210113000670420'       #申请的APPID
-    From_Data['salt']='1435660288'        #随机数
+    From_Data['salt']=str(random.randint(32768,65536))        #随机数
     Key='UK9njd0CMJn7xNPbzLT4'                    #平台分配的密匙
     m=From_Data['appid']+content+From_Data['salt']+Key
-    m_MD5=hashlib.md5(m.encode('utf8'))
+    m_MD5=hashlib.md5(m.encode("utf-8"))
     From_Data['sign']=m_MD5.hexdigest()
-
+    #print(From_Data)
     data=urllib.parse.urlencode(From_Data).encode('utf-8')  #使用urlencode()方法转换标准格式
     response=urllib.request.urlopen(URL,data)            #传递request对象和转换完格式的数据
     html=response.read().decode('utf-8')          #读取信息并解码
     translate_results=json.loads(html)            #使用JSON
-    #print(translate_results)                      #打印出JSON数据
+    print(translate_results)                      #打印出JSON数据
     translate_results=translate_results['trans_result'][0]['dst']   #找到翻译结果
     print('翻译的结果是: %s'%translate_results)               #打印翻译信息
     return translate_results
 
+#有道翻译
 def translate_youdao(content,sourse,destination):
     url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
     data = {}
     data['i'] = content #翻译的内容
     data['doctype'] = 'json'  
     data['from'] = sourse #翻译前的语种 
-    data['to'] = destination    #翻译后的语种
+    data['to'] =  destination   #翻译后的语种
+    #print(data)
     data = urllib.parse.urlencode(data).encode('utf-8')
     response = urllib.request.urlopen(url,data)
     html = response.read().decode('utf-8')
@@ -42,14 +46,32 @@ def translate_youdao(content,sourse,destination):
     print("result: %s" %result)
     return result
 
+#谷歌翻译
 def translate_google(content,sourse,destination):
     translator = google_translator()
     From_Data={}  #创建From_Data字典，存储向服务器发送的data
     From_Data['from']=sourse #输入的语种
     From_Data['to']=destination #翻译的语种
     text = translator.translate(content,lang_src=From_Data['from'],lang_tgt=From_Data['to'])
-    print(text)
+    #print(text)
     return text
+
+
+
+if __name__ == '__main__':
+    content = "我喜欢你" #翻译内容
+    sourse = 'zh' #源语言
+    destination = 'de' #目标语言
+    #translate_google(content,sourse,destination)
+    #translate_youdao(content,sourse,destination)
+    translate_baidu(content,sourse,destination)
+    #destination = 'ru'
+    #translate_google(content,sourse,destination)
+    #translate_youdao(content,sourse,destination)
+    time.sleep(1)
+    translate_baidu(content,sourse,destination)
+
+
 
 
 
